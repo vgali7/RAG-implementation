157b143e6d4eaadd68b3556a47c692d80fb5306b
vishalg1359 <129417531+vishalg1359@users.noreply.github.com>
2024-06-21 12:50:40 -0400

    Update app.py

--- Experimentation/app.py
+++ Experimentation/app.py
@@ -18,6 +18,7 @@ def login():
     if st.button("Login"):
         if username == "admin" and password == "coffee":
             st.session_state['logged_in'] = True
+            st.experimental_rerun()
         else:
             st.error("Invalid username or password")
 
