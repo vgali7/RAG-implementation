fcc0429cb24b5a921be922ae4c9c70f27fcf9276
vishalg1359 <129417531+vishalg1359@users.noreply.github.com>
2024-08-09 18:30:56 -0400

    Update model_base.py

--- Experimentation/model_base.py
+++ Experimentation/model_base.py
@@ -13,11 +13,12 @@ from elasticsearch import Elasticsearch
 from langchain.docstore.document import Document
 import streamlit as st
 import os 
-os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
+os.environ['OPENAI_API_KEY'] = api_key
 
 class Model:
     def __init__(self):
-        os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
+        
         self.question = None
         self.llm = ChatOpenAI(model_name="gpt-3.5-turbo")
         self.llm = ChatOpenAI(model_name="gpt-4o")
